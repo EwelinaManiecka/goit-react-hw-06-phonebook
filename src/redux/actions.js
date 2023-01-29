0b3939bf33de60e26ff5476c89c69695b4c1fb60
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-01-29 17:06:37 +0100

    Update actions.js
    name deleteActions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
 
 export const addContact = createAction('add_contact');
-export const deleteContact = createAction('delete_contack');
+export const deleteContact = createAction('delete_contact');
 export const setFilter = createAction('set_filter');
